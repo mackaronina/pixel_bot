cb3271f722b2b7cfa053ea8c14a8f107c3790404
mackaronina <116784694+mackaronina@users.noreply.github.com>
2024-11-14 06:34:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -18,41 +18,11 @@ ME = 7258570440
 SERVICE_CHATID = -1002171923232
 token = os.environ['BOT_TOKEN']
 APP_URL = f'https://pixel-bot-5lns.onrender.com/{token}'
-PPFUN_URL = "https://pixmap.fun"
-COLORS = [
-    (202, 227, 255),
-    (255, 255, 255),
-    (255, 255, 255),
-    (228, 228, 228),
-    (196, 196, 196),
-    (136, 136, 136),
-    (78, 78, 78),
-    (0, 0, 0),
-    (244, 179, 174),
-    (255, 167, 209),
-    (255, 84, 178),
-    (255, 101, 101),
-    (229, 0, 0),
-    (154, 0, 0),
-    (254, 164, 96),
-    (229, 149, 0),
-    (160, 106, 66),
-    (96, 64, 40),
-    (245, 223, 176),
-    (255, 248, 137),
-    (229, 217, 0),
-    (148, 224, 68),
-    (2, 190, 1),
-    (104, 131, 56),
-    (0, 101, 19),
-    (202, 227, 255),
-    (0, 211, 221),
-    (0, 131, 199),
-    (0, 0, 234),
-    (25, 25, 115),
-    (207, 110, 228),
-    (130, 0, 128)
-]
+
+URL = 'pixmap.fun'
+X = 3515
+Y = -13294
+FILE = 'shablon.png'
 
 
 class ExHandler(telebot.ExceptionHandler):
@@ -74,7 +44,7 @@ bot.set_webhook(url=APP_URL, allowed_updates=['message', 'callback_query', 'chat
 cursor = create_engine(
     f'postgresql://postgres.hdahfrunlvoethhwinnc:gT77Av9pQ8IjleU2@aws-0-eu-central-1.pooler.supabase.com:5432/postgres',
     pool_recycle=280)
-db = []
+DB_CHATS = [-1002171923232, -1002037657920]
 
 
 class Matrix:
@@ -110,21 +80,37 @@ class Matrix:
             self.matrix[y - self.start_y][x - self.start_x] = [color[0], color[1], color[2], 255]
 
 
-def fetch(session, canvas_id, canvasoffset, ix, iy, target_matrix):
-    url = f"{PPFUN_URL}/chunks/{canvas_id}/{ix}/{iy}.bmp"
+def fetch_me():
+    url = f"{URL}/api/me"
+    with requests.Session() as session:
+        attempts = 0
+        while True:
+            try:
+                resp = session.get(url, impersonate="chrome110")
+                data = resp.json()
+                return data
+            except:
+                if attempts > 3:
+                    print(f"Could not get {url} in three tries, cancelling")
+                    raise
+                attempts += 1
+                print(f"Failed to load {url}, trying again in 5s")
+                time.sleep(3)
+                pass
+
+
+def fetch(sess, canvas_id, canvasoffset, ix, iy, target_matrix, colors):
+    url = f"{URL}/chunks/{canvas_id}/{ix}/{iy}.bmp"
     attempts = 0
     while True:
         try:
-            rsp = session.get(url, impersonate="chrome110")
+            rsp = sess.get(url, impersonate="chrome110")
             data = rsp.content
             offset = int(-canvasoffset * canvasoffset / 2)
             off_x = ix * 256 + offset
             off_y = iy * 256 + offset
             if len(data) == 0:
-                for i in range(256 * 256):
-                    tx = off_x + i % 256
-                    ty = off_y + i // 256
-                    target_matrix.set_pixel(tx, ty, COLORS[0])
+                raise Exception("len(data) == 0")
             else:
                 i = 0
                 for b in data:
@@ -132,12 +118,12 @@ def fetch(session, canvas_id, canvasoffset, ix, iy, target_matrix):
                     ty = off_y + i // 256
                     bcl = b & 0x7F
 
-                    if 0 <= bcl <= 31:
-                        target_matrix.set_pixel(tx, ty, COLORS[bcl])
+                    if 0 <= bcl < len(colors):
+                        color = colors[bcl]
+                        target_matrix.set_pixel(tx, ty, color)
                     i += 1
             break
-        except Exception as e:
-            bot.send_message(ME, str(e))
+        except:
             if attempts > 3:
                 print(f"Could not get {url} in three tries, cancelling")
                 raise
@@ -147,7 +133,7 @@ def fetch(session, canvas_id, canvasoffset, ix, iy, target_matrix):
             pass
 
 
-def get_area(canvas_id, canvas_size, x, y, w, h):
+def get_area(canvas_id, canvas_size, x, y, w, h, colors):
     target_matrix = Matrix()
     target_matrix.add_coords(x, y, w, h)
     canvasoffset = math.pow(canvas_size, 0.5)
@@ -156,59 +142,56 @@ def get_area(canvas_id, canvas_size, x, y, w, h):
     wc = (x + w - offset) // 256
     yc = (y - offset) // 256
     hc = (y + h - offset) // 256
-    print(f"Loading from {xc} / {yc} to {wc + 1} / {hc + 1} PixelGetter")
     with requests.Session() as session:
+        threads = []
         for iy in range(yc, hc + 1):
             for ix in range(xc, wc + 1):
-                fetch(session, canvas_id, canvasoffset, ix, iy, target_matrix)
+                time.sleep(0.01)
+                t = Thread(target=fetch, args=(session, canvas_id, canvasoffset, ix, iy, target_matrix, colors))
+                t.start()
+                threads.append(t)
+        for t in threads:
+            t.join()
         return target_matrix.create_image()
 
 
-def convert_color(color):
+def convert_color(color, colors):
     dists = []
-    for c in COLORS:
+    for c in colors:
         d = math.sqrt(
             math.pow(int(color[0]) - c[0], 2) + math.pow(int(color[1]) - c[1], 2) + math.pow(int(color[2]) - c[2], 2))
         dists.append(d)
-    return COLORS[dists.index(min(dists))]
+    return colors[dists.index(min(dists))]
 
 
 def get_difference():
-    img = np.array(PIL.Image.open(r"shablon.png"), dtype='uint8')
-    shablon_x = 3515
-    shablon_y = -13294
+    img = np.array(PIL.Image.open(FILE), dtype='uint8')
     shablon_w = img.shape[1]
     shablon_h = img.shape[0]
-    img1 = get_area(0, 65536, shablon_x, shablon_y, shablon_w, shablon_h)
+    canvas = fetch_me()["canvases"]["0"]
+    colors = list(map(canvas["colors"], tuple))
+    map_img = get_area(0, canvas["size"], X, Y, shablon_w, shablon_h, colors)
     show_diff = np.zeros((shablon_h, shablon_w, 4), dtype='uint8')
-    show_diff[0][0] = [255, 0, 0, 255]
     total_size = 0
     diff = 0
     for x in range(shablon_h):
         for y in range(shablon_w):
-            if img[x][y][3] == 0:
+            if img[x][y][3] < 255:
                 continue
-            if (img[x][y][0], img[x][y][1], img[x][y][2]) not in COLORS:
-                color = convert_color(img[x][y])
+            if (img[x][y][0], img[x][y][1], img[x][y][2]) not in colors:
+                color = convert_color(img[x][y], colors)
             else:
                 color = img[x][y]
-            if color[0] != img1[x][y][0] or color[1] != img1[x][y][1] or color[2] != img1[x][y][2]:
+            if color[0] != map_img[x][y][0] or color[1] != map_img[x][y][1] or color[2] != map_img[x][y][2]:
                 diff += 1
-                show_diff[x][y] = [img1[x][y][0], img1[x][y][1], img1[x][y][2], 255]
+                show_diff[x][y] = [map_img[x][y][0], map_img[x][y][1], map_img[x][y][2], 255]
+            else:
+                show_diff[x][y] = [0, 255, 0, 255]
             total_size += 1
+    del map_img
     del img
-    del img1
-    img2 = PIL.Image.new('RGBA', (shablon_w, shablon_h), (255, 0, 0, 0))
-    pxls = img2.load()
-    for x in range(shablon_h):
-        for y in range(shablon_w):
-            try:
-                if show_diff[x][y][3] > 0:
-                    pxls[y, x] = (show_diff[x][y][0], show_diff[x][y][1], show_diff[x][y][2], 255)
-            except (IndexError, KeyError, AttributeError):
-                pass
-    # img2.save("difference.png")
-    return (total_size - diff) / total_size, diff, send_pil(img2)
+    show_diff = PIL.Image.fromarray(show_diff).convert('RGBA')
+    return (total_size - diff) / total_size, diff, send_pil(show_diff)
 
 
 def send_pil(im):
@@ -224,23 +207,19 @@ def to_fixed(f: float, n=0):
     return '{}.{}{}'.format(a, b[:n], '0' * (n - len(b)))
 
 
-def handle_text(message, txt):
-    print('Сообщение получено')
-    if message.chat.id not in db:
-        db.append(message.chat.id)
-        cursor.execute(f'INSERT INTO ukr_chats (id) VALUES ({message.chat.id})')
-
-
 @bot.message_handler(commands=["testo"])
 def msg_testo(message):
-    bot.send_message(ME, 'abba')
-    job_hours()
+    perc, diff, img = get_difference()
+    m = bot.send_document(SERVICE_CHATID, img)
+    fil = m.document.file_id
+    text = f"На {URL} Україна співпадає з шаблоном на {to_fixed(perc * 100, 2)} %\nПікселів не за шаблоном: {diff}"
+    bot.send_message(SERVICE_CHATID, text)
+    bot.send_document(SERVICE_CHATID, fil, caption="Зеленим пікселі за шаблоном, іншими кольорами - ні")
 
 
 @bot.chat_member_handler()
 def msg_chat(upd):
-    if upd.new_chat_member.status == "member" and (
-            upd.old_chat_member.status == "left" or upd.old_chat_member.status == "kicked"):
+    if upd.new_chat_member.status == "member" and upd.old_chat_member.status == "left":
         bot.send_animation(upd.chat.id,
                            'CgACAgQAAyEFAASBdOsgAAIV-Wc0pgq0nWuUz2g9vOV_U8qwONWbAAK9BQAC3_skU_chjKqyZotRNgQ')
 
@@ -253,10 +232,6 @@ def msg_text(message):
                          reply_to_message_id=message.message_id)
     if message.chat.id == SERVICE_CHATID and message.animation is not None:
         bot.send_message(message.chat.id, str(message.animation.file_id), reply_to_message_id=message.message_id)
-    if message.text is not None:
-        handle_text(message, message.text)
-    elif message.caption is not None:
-        handle_text(message, message.caption)
 
 
 @app.route('/' + token, methods=['POST'])
@@ -282,31 +257,21 @@ def updater():
 def job_hours():
     perc, diff, img = get_difference()
     bot.send_message(ME, 'abba2')
-    # with open("difference.png", 'rb') as img:
     m = bot.send_document(SERVICE_CHATID, img)
     fil = m.document.file_id
-    text = f"На пм Україна співпадає з шаблоном на {to_fixed(perc * 100, 2)} %\nПікселів не за шаблоном: {diff}"
-    for chatid in db:
+    text = f"На {URL} Україна співпадає з шаблоном на {to_fixed(perc * 100, 2)} %\nПікселів не за шаблоном: {diff}"
+    for chatid in DB_CHATS:
         try:
             bot.send_message(chatid, text)
-            bot.send_document(chatid, fil, caption="На картинці всі пікселі не за шаблоном")
+            bot.send_document(chatid, fil,
+                              caption="Зеленим пікселі за шаблоном, іншими кольорами - ні. Використовуй цю мапу щоб знайти пікселі, які потрібно замалювати")
         except:
             pass
 
 
-def init_db():
-    data = cursor.execute('SELECT id FROM ukr_chats')
-    data = data.fetchall()
-    if data is not None:
-        for dat in data:
-            db.append(dat[0])
-    print(db)
-
-
 if __name__ == '__main__':
-    init_db()
     bot.send_message(ME, "ok")
-    schedule.every(3).hours.do(job_hours)
-    t = Thread(target=updater)
-    t.start()
+    schedule.every(110).minutes.do(job_hours)
+    thr = Thread(target=updater)
+    thr.start()
     app.run(host='0.0.0.0', port=80, threaded=True)
