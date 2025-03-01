e6cbaff5a2604b4e36e03e2626b52f16e277bc60
mishurov2021ks14 <mishurov2021ks14@student.karazin.ua>
2025-03-01 11:45:11 +0200

    Some commit

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ VOID_TOPIC = 37
 COORDINATION_TOPIC = [18, 19]
 TOKEN = os.environ['BOT_TOKEN']
 DB_URL = os.environ['DB_URL']
-APP_URL = f'https://pixel-bot-5lns.onrender.com/{TOKEN}'
+APP_URL = f'https://pixel-bot-2.onrender.com/{TOKEN}'
 DB_CHATS = [SERVICE_CHATID, MAIN_CHATID]
 SITES_VIA_PROXY = ['pixelplanet.fun', 'pixuniverse.xyz', 'pixuniverse.fun']
